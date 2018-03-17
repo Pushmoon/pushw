3d93ec438842db3afe323745abc87d735baa2f0b
Pushmoon <13262203191@163.com>
2018-03-17 13:52:07 +0800

    add config

---
+++ app/src/main/java/com/pushw/android/db/city.java
@@ -0,0 +1,46 @@
+package com.pushw.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ruby on 2018/3/17.
+ */
+
+public class city extends DataSupport {
+    private int id;
+    private String cityname;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityname() {
+        return cityname;
+    }
+
+    public void setCityname(String cityname) {
+        this.cityname = cityname;
+    }
+
+    public int getCityCode(){
+        return  cityCode;
+    }
+
+    public void setCityCode(int cityCode){
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId(){
+        return  provinceId;
+    }
+
+    public void setProvinceId(int provinceId){
+        this.provinceId = provinceId;
+    }
+}
